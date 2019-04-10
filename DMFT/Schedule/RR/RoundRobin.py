cd73f0795ba058db8da0360234e33d1394ae3919
Ashfaq <er.ashfaq.memon@gmail.com>
2019-04-10 10:50:13 +0800

    Very important commit. Round robin commented and improved.

--- DMFT/Schedule/RR/RoundRobin.py
+++ DMFT/Schedule/RR/RoundRobin.py
@@ -1,47 +1,38 @@
+'''
+NUS - EE5903 - Real Time Systems
+
+Author - Aspak Rogatiya (A0179741U)
+
+Simulation: Round Robin Algorithm with Fault Tolerance
+
+Task Model:
+        Periodic (WCET <= Deadline <= Period)
+        All task start to arrive at time 0.
+Fault Model:
+        MTTF
+'''
+
+"""
+Imports
+"""
 import fractions
-import time
 from collections import OrderedDict
 import matplotlib.pyplot as plt
 import numpy as np
 
-IDLE=100
 DEADLINEMISS=500
 USE=1
-def _lcm(a,b): return abs(a * b) / fractions.gcd(a,b) if a and b else 0
 
-def lcm(a):
-    return reduce(_lcm, a)
-
-def avg(a, b):
-    return (a + b) / 2.0
-
-chart = []
-
-avC=0 #average execution time
-avD=0 #average deadline
-
-tq = 4
-hyperperiod = []
-util = 0
-tt = OrderedDict()
-missedDeadlines = 0
-
-def print_taskset(tasks):
-    textStr = "Task\tC\tD\tP\n"
-    textStr += "------------------------------\n" 
-    for task in tasks:
-        textStr += str(task)
-    textStr += "------------------------------\n"
-    return textStr
-
-class Process:
-    def __init__(self,pid,AT,BT,deadline,period,color):
-        self.pid = pid
-        self.arrival = AT
-        self.burst = BT
-        self.bkpburst = BT
-        self.period = period
-        self.deadline = deadline
+"""
+A Task class.
+"""
+class Task:
+    def __init__(self,name,C,D,P,color):
+        self.name = name
+        self.C = C
+        self.bkpC = C
+        self.P = P
+        self.D = D
         self.color = color
         self.sc = 0 #shortness component
         self.pc = 0 #priority component
@@ -53,21 +44,59 @@ class Process:
         self.instDone = 0 #number of instances done
 
     def __str__(self):
-        return self.pid + "\t" + str(self.bkpburst) + "\t" + str(self.deadline) + "\t" + str(self.period) + "\n"
+        return self.name + "\t" + str(self.bkpC) + "\t" + str(self.D) + "\t" + str(self.P) + "\n"
 
+"""
+Global variables
+"""
+#chart = []
+avC=0 #average execution time
+avD=0 #average D
+tq = 4 #original time quantum
+hyperperiod = []
+util = 0 #CPU utilization 
+tt = OrderedDict() # Time table. 
+missedDeadlines = 0 # Number of deadlines missed during this simulation
+
+
+"""
+Useful function definitions
+"""
+def _lcm(a,b): 
+    return abs(a * b) / fractions.gcd(a,b) if a and b else 0
+
+def lcm(a):
+    return reduce(_lcm, a)
+
+def avg(a, b):
+    return (a + b) / 2.0
+
+def print_taskset(tasks):
+    textStr = "Task\tC\tD\tP\n"
+    textStr += "------------------------------\n" 
+    for task in tasks:
+        textStr += str(task)
+    textStr += "------------------------------\n"
+    return textStr
         
-def shiftCL(plist):
-    temp = plist[0]
-    for i in range(len(plist)-1):
-        plist[i] = plist[i+1]
-    plist[len(plist)-1] = temp
-    return plist
+def shiftLeftQueue(taskList):
+    temp = taskList[0]
+    for i in range(len(taskList)-1):
+        taskList[i] = taskList[i+1]
+    taskList[len(taskList)-1] = temp
+    return taskList
 
-def RR(plist,n):
-    global chart
+def RR(taskList):
+    """
+    Implementation of Round-Robin function.
+    """
+    #===========================================================================
+    # Local and global variable declarations
+    #===========================================================================
+    n = len(taskList) # Number of tasks in task list
+    #global chart
     queue = []
-    t = 0
-    ap = 0
+    time = 0
     rp = 0
     done = 0
     start = 0
@@ -78,127 +107,196 @@ def RR(plist,n):
     global util
     global missedDeadlines
     missFlag = False
-    #calculate average execution time
-    for p in plist:
-        avC+=p.burst
-        avD+=p.deadline
+
+    #===========================================================================
+    # Calculate avC (average execution time) and avD (average deadline)
+    #===========================================================================
+    for t in taskList:
+        avC+=t.C
+        avD+=t.D
     avC/=n
     avD/=n
     print "Average Burst:",avC
     print "Average Deadline:",avD
 
-    for p in plist:
-        util += float(p.burst)/float(p.period)
-        hyperperiod.append(p.period)
+    #===========================================================================
+    # Calculate hyperperiod and utilization
+    #===========================================================================
+    for t in taskList:
+        util += float(t.C)/float(t.P)
+        hyperperiod.append(t.P)
     hyperperiod = lcm (hyperperiod)
-    print "hyperperiod:\t{}".format(hyperperiod)
-    print "utilization:\t{}".format(util)
+    print "Hyperperiod:\t{}".format(hyperperiod)
+    print "Utilization:\t{}".format(util)
     
-    for p in plist:
-        if p.burst <= avC:
-            p.sc = 1
-        if p.deadline <= avD:
-            p.pc = 1
-        p.cc = tq+p.pc+p.sc
-        p.bcb = p.burst - p.cc
-        if p.bcb < tq:
-            p.csc = p.bcb
-        p.q = tq+p.pc+p.sc+p.csc
-        print "T:{}\tsc:{}\tpc:{}\tcc:{}\tbcb:{}\tcsc:{}\tits:{}".format(p.pid,p.sc,p.pc,p.cc,p.bcb,p.csc,p.q)
+    #===========================================================================
+    # Calculate Intelligent Time Slice (ITS) for Round Robin to avoid very
+    # frequent context switches in RTOS.
+    # Reference paper : https://www.arpapress.com/Volumes/Vol2Issue2/IJRRAS_2_2_06.pdf
+    # sc = Shortness Component
+    # pc = Priority Component
+    # bcb = Balanced Computation Burst
+    # tq = original time quantum
+    # q = calculated intelligent time quantum
+    #===========================================================================    
+    for t in taskList:
+        if t.C <= avC:
+            t.sc = 1
+        if t.D <= avD:
+            t.pc = 1
+        t.cc = tq+t.pc+t.sc
+        t.bcb = t.C - t.cc
+        if t.bcb < tq:
+            t.csc = t.bcb
+        t.q = tq+t.pc+t.sc+t.csc
+        print "T:{}\tsc:{}\tpc:{}\tcc:{}\tbcb:{}\tcsc:{}\tits:{}".format(t.name,t.sc,t.pc,t.cc,t.bcb,t.csc,t.q)
         
-        p.inst = hyperperiod/p.period
+        # number of instances of this task throughout the hyperperiod
+        t.inst = hyperperiod/t.P
     
-    #for i in range(len(task_types)):
-    for p in plist:
-        tt[p.pid] = [[0]*hyperperiod,p.color]
+    #===========================================================================
+    # Create a time table of hyperperiod x n. Initially all 0. Used time instances
+    # will be marked as 1. Missed deadline will be marked as 500. 
+    #===========================================================================
+    for t in taskList:
+        tt[t.name] = [[0]*hyperperiod,t.color]
 
-    for item in tt:
-        print tt[item]
-        
-    while t < hyperperiod:
-        for p in plist:
-            if (t - p.instDone * p.period) >= 0:
-                p.burst = p.bkpburst
-                queue.append(p)
-                p.instDone += 1
-                ap += 1
+    #===========================================================================
+    # Start scheduling the task list over the hyper period in round robin fashion
+    #===========================================================================
+    while time < hyperperiod:
+        # Check for all the "new" tasks arrived on or before this time.
+        for t in taskList:
+            if (time - t.instDone * t.P) >= 0:
+                t.C = t.bkpC
+                queue.append(t)
+                t.instDone += 1
                 rp += 1
         
+        # No remaining processes.. CPU is idle.
         if rp < 1:
-            chart.append(0)
-            t += 1
+            #chart.append(0)
+            time += 1
             continue
         
+        # Scheduling has started. Shift left to pick the first one in the queue.
         if start:
-            queue = shiftCL(queue)
+            queue = shiftLeftQueue(queue)
         
-        #pick the first task from queue and put it on cpu
+        # Pick the first task from queue and put it on CPU
         on_cpu = queue[0]
+        # Assume that deadline won't be missed.
         missFlag = False
-        if on_cpu.burst > 0 :
-            if on_cpu.burst > on_cpu.q:
-                for i in range(t, t+on_cpu.q):
-                    chart.append(on_cpu.pid)
-                    if i >= ((on_cpu.instDone - 1) * on_cpu.period + on_cpu.deadline):
-                        tt[on_cpu.pid][0][i] = DEADLINEMISS
+        # Is there an executable task on CPU?
+        if on_cpu.C > 0 :
+            # Yes there is. Is the time of this task bigger than time quantum?
+            if on_cpu.C > on_cpu.q:
+                # Yes it is. Execute for the assigned time quantum and put the remaining at
+                # the end of queue.
+                for i in range(time, time+on_cpu.q):
+                    #chart.append(on_cpu.name)
+                    #Is the deadline missed already?
+                    if i >= ((on_cpu.instDone - 1) * on_cpu.P + on_cpu.D):
+                        # yes, mark this time instance as "missed deadline" on the time table
+                        tt[on_cpu.name][0][i] = DEADLINEMISS
                         missFlag = True
                     else:
-                        tt[on_cpu.pid][0][i] = USE
-                on_cpu.burst -= on_cpu.q
+                        # No. We did it before deadline. Mark this time instance as a "success" on time table
+                        tt[on_cpu.name][0][i] = USE
+                on_cpu.C -= on_cpu.q
             else:
-                for i in range(t,t+on_cpu.burst):
-                    chart.append(on_cpu.pid)
-                    if i >= ((on_cpu.instDone - 1) * on_cpu.period + on_cpu.deadline):
-                        tt[on_cpu.pid][0][i] = DEADLINEMISS
+                # No, time quantum is sufficient for this task. Just execute it and get rid of it from the queue.
+                for i in range(time,time+on_cpu.C):
+                    #chart.append(on_cpu.name)
+                    # Is the deadline missed already?
+                    if i >= ((on_cpu.instDone - 1) * on_cpu.P + on_cpu.D):
+                        # yes, this time instance as "missed deadline" on the time table
+                        tt[on_cpu.name][0][i] = DEADLINEMISS
                         missFlag = True
                     else:
-                        tt[on_cpu.pid][0][i] = USE
-                t+=on_cpu.burst
-                on_cpu.burst = 0
+                        # No. We did it before deadline. Mark this time instance as a "success" on time table
+                        tt[on_cpu.name][0][i] = USE
+                time+=on_cpu.C
+                on_cpu.C = 0
                 done+=1
                 rp -= 1
-            if missFlag:                
+            # Was a deadline missed? 
+            if missFlag:
+                # Yes. We missed one. 
                 missedDeadlines += 1
             start=1
             
         else:
+            # remove a "done" task from queue.
             queue.remove(on_cpu)
 
+#===============================================================================
+# Start execution from here
+#===============================================================================
 if __name__ == '__main__':
+    
+    # Read task list from taskfile.txt
     taskfile = open('taskfile.txt','r')
     tasklines = taskfile.readlines()
-    task_types = []
+    taskList = []
     
     try:
         for line in tasklines:
             line = line.split('\t')
             name = line[0]
             color = line[4][:-1]
-            c = int(line[1])
-            d = int(line[2])
-            p = int(line[3])
-            task_types.append(Process(pid=name, AT=0, BT=c,deadline=d,period=p,color=color))
+            C = int(line[1])
+            D = int(line[2])
+            P = int(line[3])
+            taskList.append(Task(name=name, C=C,D=D,P=P,color=color))
     except Exception as exc:
         print "Invalid task file structure. Error: ", exc
+        print "expected task structure as: ", "Task\tC\tD\tP\n"
 
-    RR(task_types,len(task_types))
+    #Schedule this task list in round-robin fashion
+    RR(taskList)
     
-    for i in range(len(chart)):
-        if(i and (chart[i]!=chart[i-1])):
-            print "\n"
-        print "t: {}\t{}".format(i, chart[i])
-
-    print chart
+    #===============================================================================
+    #     for i in range(len(chart)):
+    #         if(i and (chart[i]!=chart[i-1])):
+    #             print "\n"
+    #         print "t: {}\t{}".format(i, chart[i])
+    # 
+    #     print chart
+    #===============================================================================
     for item in tt:
         print tt[item]
 
+    
+    #plot properties
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.axes.get_yaxis().set_visible(True)
-    ax.set_aspect(1)
-    plt.grid(False)
+    ax.set_aspect(2.5)
+
+    # Major ticks every 2, minor ticks every 1
+    major_ticks = np.arange(0, hyperperiod+1, 2)
+    minor_ticks = np.arange(0, hyperperiod+1, 1)
+    
+    ax.set_xticks(major_ticks)
+    ax.set_xticks(minor_ticks, minor=True)
+    
+    # Or if you want different settings for the grids:
+    ax.grid(which='minor', alpha=0.2)
+    ax.grid(which='major', alpha=0.5)    
     
     for y, (name,(row,color)) in enumerate(tt.items()):
+
+        #Traverse through the hyperperiod. Mark deadline and period of each task with arrows.
+        clock_step = 1
+        for task in taskList:
+            for i in xrange(0,hyperperiod+1,clock_step):
+                if (i % task.P == task.D) and (i > 0):
+                    ax.annotate("",xy=(i,int(task.name[1])-1),xycoords= 'data',xytext=(i,int(task.name[1])),textcoords='data',
+                        arrowprops=dict(arrowstyle='simple'))
+                if (i % task.P == 0):
+                    ax.annotate("",xy=(i,int(task.name[1])),xycoords= 'data',xytext=(i,int(task.name[1])-1),textcoords='data',
+                        arrowprops=dict(arrowstyle='fancy'))
+
         for x, col in enumerate(row):
             x1 = [x, x+1]
             y1 = np.array([y, y])
@@ -208,11 +306,6 @@ if __name__ == '__main__':
                 plt.text(avg(x1[0], x1[1]), avg(y1[0], y2[0]), name, 
                                             horizontalalignment='center',
                                             verticalalignment='center')
-            if col==IDLE:
-                plt.fill_between(x1, y1, y2=y2, color='grey')
-                plt.text(avg(x1[0], x1[1]), avg(y1[0], y2[0]), "E", 
-                                            horizontalalignment='center',
-                                            verticalalignment='center')
                 
             if col==DEADLINEMISS:
                 plt.fill_between(x1, y1, y2=y2, color='yellow')
@@ -220,34 +313,15 @@ if __name__ == '__main__':
                                             horizontalalignment='center',
                                             verticalalignment='center')
                 
-        #mark deadlines and periods
-        clock_step = 1
 
-        for task in task_types:
-            for i in xrange(0,hyperperiod+1,clock_step):
-                if (i % task.period == task.deadline) and (i > 0):
-                    ax.annotate("",xy=(i,int(task.pid[1])-1),xycoords= 'data',xytext=(i,int(task.pid[1])),textcoords='data',
-                        arrowprops=dict(arrowstyle='simple'))
-                if (i % task.period == 0):
-                    ax.annotate("",xy=(i,int(task.pid[1])),xycoords= 'data',xytext=(i,int(task.pid[1])-1),textcoords='data',
-                        arrowprops=dict(arrowstyle='fancy'))
-
-    plt.xticks(np.arange(0,hyperperiod+1,2))
-    plt.yticks(np.arange(0,len(task_types)+1))
     plt.xlim(0,hyperperiod)
-    plt.ylim(0,len(task_types)+3)
+    plt.ylim(0,len(taskList))
     
-    #props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
-     
     textStr = "*** Round-Robin Scheduling ***\n"
     textStr += "--------------------------------------------\n"
-    textStr += print_taskset(task_types)
+    textStr += print_taskset(taskList)
     textStr += "U:\t{:.2f}\n".format(util)
     textStr += "Missed Deadlines: {}".format(missedDeadlines)
     textStr = textStr.expandtabs()
-    # place a text box in upper left in axes coords
-    #ax.text(0.05, 0.95, textStr, transform=ax.transAxes, fontsize=8,
-    #        verticalalignment='top', bbox=props)
     plt.title(textStr,fontdict={'fontsize': 8, 'fontweight': 'medium'},loc='left')
     plt.show()
-    #plt.savefig('foo.png', bbox_inches='tight',dpi=500)    
